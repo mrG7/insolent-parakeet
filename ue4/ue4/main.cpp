492a9af429a031b66cce5eed5649b1189ff04ee5
Byrdann Fox <mobileupstate@gmail.com>
2015-10-02 03:45:23 -0400

    asdfjkl;

---
+++ ue4/ue4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ue4
+//
+//  Created by Byrdann Fox on 10/2/15.
+//  Copyright © 2015 Loopi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
